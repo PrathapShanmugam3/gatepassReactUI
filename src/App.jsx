9592cebaa112d87197fc4c80c9ba5c73c68b6cdf
prathapshanmugam3 <prathapshanmugam5@gmail.com>
2025-03-05 21:43:20 +0530

    Fix deployment issues

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Routes>
+        <Routes  basename="/gatepassReactUI">
           <Route path="/" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
