0acf3ab5fc3b60328deb23caf87dc225af976890
prathapshanmugam3 <prathapshanmugam5@gmail.com>
2025-03-06 10:35:52 +0530

    path changed

--- src/Api.jsx
+++ src/Api.jsx
@@ -2,7 +2,9 @@
 import axios from "axios";
 
 // Set base URL for all API calls
-const baseURL = "http://localhost:10000"; // Replace with your actual backend URL
+// const baseURL = "http://localhost:10000"; // Replace with your actual backend URLhttps://gatepassapiserver.onrender.com/
+
+const baseURL = "https://gatepassapiserver.onrender.com";
 
 const Api = axios.create({
     baseURL: baseURL,
